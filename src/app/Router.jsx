75426b63ca52c5e398b16497192c7bee0ec7c6c5
vsevolod <sevostsev@gmail.com>
2024-10-31 18:14:28 +0100

    finished

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TeamsPage from "../pages/TeamsPage";
 import TeamPage from "../pages/TeamPage";
+import TeamSelectorPage from "../pages/TeamSelectorPage";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route index element={<TeamsPage />} />
       <Route path="/teams/:id" element={<TeamPage />} />
+      <Route path="/teams/selector" element={<TeamSelectorPage />} />
 
       {/* Es muy recomendable añadir esta ruta para obtener un mensaje de error en el caso de que la ruta no exista. De lo contrario, si la ruta no existe llegaremos a una página en blanco */}
       <Route path="*" element={<div>404</div>} />
